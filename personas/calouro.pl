d6a3773acebd88890a1b89093e9d84be35701dc3
Ballen <71524526+The-Ballen@users.noreply.github.com>
2025-11-27 02:10:32 -0300

    Introdução de templates de personas

---
+++ personas/calouro.pl
@@ -0,0 +1,46 @@
+% ==========================================
+% PERFIL ATUALIZADO PELO STICC
+% ==========================================
+:- retractall(sabe(_)).
+
+% ----- 1º Período -----
+% Cálculo Diferencial e Integral I:
+sabe(funcoes_reais).
+sabe(limites_continuidade).
+sabe(derivadas_regras).
+
+% Geometria Analítica:
+sabe(vetores_operacoes).
+sabe(retas_equacoes).
+sabe(distancias).
+
+% Introdução à Ciência da Computação:
+sabe(historia_evolucao_computadores).
+sabe(sistemas_numeracao).
+sabe(arquitetura_von_neumann).
+sabe(software_basico).
+sabe(etica_informatica).
+sabe(nocoes_redes_ia).
+
+% Introdução à Programação:
+sabe(algoritmos_basicos).
+sabe(tipos_dados_c).
+sabe(estruturas_controle).
+sabe(vetores_matrizes).
+
+% Metodologia Científica:
+sabe(metodo_cientifico).
+sabe(tipos_pesquisa).
+sabe(projeto_pesquisa).
+
+% Matemática Discreta:
+sabe(logica_proposicional).
+sabe(logica_predicados).
+sabe(relacoes_funcoes).
+
+
+% ----- 7º Período -----
+% Trabalho de Conclusão de Curso I:
+sabe(projeto_pesquisa).
+
+
---
+++ personas/veterano.pl
@@ -0,0 +1,114 @@
+% ==========================================
+% PERFIL ATUALIZADO PELO STICC
+% ==========================================
+:- retractall(sabe(_)).
+
+% ----- 1º Período -----
+% Cálculo Diferencial e Integral I:
+sabe(funcoes_reais).
+sabe(limites_continuidade).
+sabe(derivadas_regras).
+sabe(aplicacoes_derivada).
+sabe(integrais_indefinidas).
+sabe(teorema_fundamental_calculo).
+
+% Geometria Analítica:
+sabe(vetores_operacoes).
+sabe(retas_equacoes).
+sabe(planos_equacoes).
+sabe(distancias).
+sabe(conicas).
+
+% Introdução à Ciência da Computação:
+sabe(historia_evolucao_computadores).
+sabe(sistemas_numeracao).
+sabe(arquitetura_von_neumann).
+sabe(software_basico).
+sabe(etica_informatica).
+sabe(nocoes_redes_ia).
+
+% Introdução à Programação:
+sabe(algoritmos_basicos).
+sabe(tipos_dados_c).
+sabe(estruturas_controle).
+sabe(vetores_matrizes).
+sabe(strings).
+sabe(structs).
+sabe(funcoes_procedimentos).
+sabe(ponteiros_alocacao).
+sabe(arquivos).
+
+% Metodologia Científica:
+sabe(metodo_cientifico).
+sabe(tipos_pesquisa).
+sabe(projeto_pesquisa).
+sabe(normas_abnt_citacoes).
+sabe(etica_plagio).
+sabe(artigo_cientifico).
+
+% Matemática Discreta:
+sabe(logica_proposicional).
+sabe(logica_predicados).
+sabe(teoria_conjuntos).
+sabe(relacoes_funcoes).
+sabe(inducao_recursao).
+sabe(estruturas_algebricas).
+
+
+% ----- 2º Período -----
+% Administração para Ciência da Computação:
+sabe(teoria_geral_adm).
+sabe(estrutura_organizacional).
+sabe(gestao_qualidade).
+sabe(modelagem_negocios).
+sabe(lideranca_motivacao).
+sabe(governanca_ti).
+
+% Álgebra Linear:
+sabe(espacos_vetoriais).
+sabe(transformacoes_lineares).
+sabe(autovalores_autovetores).
+sabe(diagonalizacao).
+sabe(produto_interno).
+sabe(operadores_especiais).
+
+% Cálculo Diferencial e Integral II:
+sabe(tecnicas_integracao).
+sabe(aplicacoes_integral).
+sabe(integrais_improprias).
+sabe(funcoes_varias_variaveis).
+sabe(derivadas_parciais).
+sabe(maximos_minimos_lagrange).
+sabe(integrais_multiplas).
+
+% Física I para Computação:
+sabe(medidas_vetores_fisicos).
+sabe(cinematica_vetorial).
+sabe(leis_newton_dinamica).
+sabe(trabalho_energia).
+
+% Lógica para Ciência da Computação:
+sabe(deducao_natural).
+sabe(sistemas_axiomaticos).
+sabe(resolucao_proposicional).
+sabe(logica_primeira_ordem).
+sabe(clausulas_horn_resolucao).
+sabe(programacao_logica_prolog).
+sabe(logicas_nao_classicas).
+
+% Sistemas de Informação:
+sabe(conceitos_dados_info).
+sabe(tipos_sistemas_info).
+sabe(etica_privacidade_si).
+sabe(infraestrutura_ti).
+sabe(seguranca_informacao).
+sabe(sistemas_empresariais).
+sabe(inteligencia_negocios).
+sabe(ciclo_vida_sistemas).
+
+
+% ----- 7º Período -----
+% Trabalho de Conclusão de Curso I:
+sabe(projeto_pesquisa).
+
+
